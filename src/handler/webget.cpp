d65b85d0b99c144baf5741e8dbabf297d1188b41
Ealrang <53871457+Ealrang@users.noreply.github.com>
2024-02-26 21:25:24 +0800

    Update webget.cpp

--- src/handler/webget.cpp
+++ src/handler/webget.cpp
@@ -183,7 +183,8 @@ static int curlGet(const FetchArgument &argument, FetchResult &result)
     header_list = curl_slist_append(header_list, "SubConverter-Request: 1");
     header_list = curl_slist_append(header_list, "SubConverter-Version: " VERSION);
     // add default user-agent
-    header_list = curl_slist_append(header_list, "User-Agent: " + std::string(user_agent_str));
+    std::string user_agent = "User-Agent: " + user_agent_str;
+    header_list = curl_slist_append(header_list, user_agent);
     if(header_list)
         curl_easy_setopt(curl_handle, CURLOPT_HTTPHEADER, header_list);
 
